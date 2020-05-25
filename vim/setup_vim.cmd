ad7946afd4a01f949712925a7184c3109efef431
linjun li <you@example.com>
2020-05-25 01:38:24 -0700

    add windows

---
+++ vim/setup_vim.cmd
@@ -0,0 +1,15 @@
+mkdir %USERPROFILE%/.vim/bundle
+git clone https://github.com/VundleVim/Vundle.vim.git %USERPROFILE%%/.vim/bundle/Vundle.vim
+
+REM mkdir %USERPROFILE%/.config/nvim
+REM touch %USERPROFILE%/.config/nvim/init.vim
+REM echo "set runtimepath^=~/.vim runtimepath+=~/.vim/after
+REM let &packpath=&runtimepath
+REM source ~/.vimrc" > %USERPROFILE%/.config/nvim/init.vim
+
+git clone https://github.com/5cr009e/vimrc.git %USERPROFILE%%/.vim_runtime
+
+mklink  %USERPROFILE%%/.vimrc .vimrc
+REM vim +PluginInstall +qall
+REM vim -E -s -u "$HOME/.vimrc" +PlugInstall +qall
+
