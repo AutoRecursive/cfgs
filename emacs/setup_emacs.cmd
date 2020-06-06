29d89dd7769422f2cd9f38a908921113daf6cb1d
LinjunLi <linjunli96@gmail.com>
2020-06-06 04:03:23 -0700

    add chemacs

--- emacs/setup_emacs.cmd
+++ emacs/setup_emacs.cmd
@@ -1,13 +1,17 @@
 @echo off
-git clone https://github.com/5cr009e/prelude.git %USERPROFILE%\.prelude.d
-mklink /J %USERPROFILE%\AppData\Roaming\.emacs.d  %USERPROFILE%\.prelude.d
+git clone https://github.com/5cr009e/prelude.git %USERPROFILE%\.prelude
+mklink /D %USERPROFILE%\AppData\Roaming\.emacs.d  %USERPROFILE%\.prelude
 
-git clone https://github.com/syl20bnr/spacemacs %USERPROFILE%\.spacemacs.d
-mklink /J %USERPROFILE%\AppData\Roaming\.spacemacs.d  %USERPROFILE%\.spacemacs.d
+git clone https://github.com/syl20bnr/spacemacs %USERPROFILE%\.spacemacs
+mklink /D %USERPROFILE%\AppData\Roaming\.spacemacs.d  %USERPROFILE%\.spacemacs
 
-REM git clone https://github.com/plexus/chemacs.git ..\..\chemacs
-REM powershell ..\..\chemacs\install.ps1
+git clone https://github.com/plexus/chemacs.git ..\..\chemacs
+powershell ..\..\chemacs\install.ps1
+
+mklink %USERPROFILE%\.emacs-profiles.el  %USERPROFILE%\.emacs-profiles.el
 REM mklink %USERPROFILE%\AppData\Roaming\.emacs  %USERPROFILE%\.emacs
 
 REM echo (("default" . ((user-emacs-directory . "~/.prelude"))) > %USERPROFILE%\AppData\Roaming\.emacs-profiles.el
 REM echo ("spacemacs" . ((user-emacs-directory . "~/.spacemacs"))) >> %USERPROFILE%\AppData\Roaming\.emacs-profiles.el
+REM echo (("default" . ((user-emacs-directory . "~/.prelude"))) > %USERPROFILE%\.emacs-profiles.el
+REM echo ("spacemacs" . ((user-emacs-directory . "~/.spacemacs"))) >> %USERPROFILE%\.emacs-profiles.el
--- emacs/setup_emacs.sh
+++ emacs/setup_emacs.sh
@@ -1,2 +1,9 @@
-export PRELUDE_URL="https://github.com/5cr009e/prelude.git" && curl -L https://github.com/bbatsov/prelude/raw/master/utils/installer.sh | sh
-emacs -nw --eval "(shell-command \"echo finished\")"
+# export PRELUDE_URL="https://github.com/5cr009e/prelude.git" && curl -L https://github.com/bbatsov/prelude/raw/master/utils/installer.sh | sh
+# emacs -nw --eval "(shell-command \"echo finished\")"
+
+git clone https://github.com/5cr009e/prelude.git ~/.prelude
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+
+git clone https://github.com/plexus/chemacs.git ~/.chemacs
+bash ~/.chemacs/install.sh
+ln -s .emacs-profiles.el ~/.emacs-profiles.el
