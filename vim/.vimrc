6a80545e3f3f16764289c3eb692f7c9e5cd9756a
Mr. Fisher <linjunlee96@163.com>
2022-12-13 22:47:13 +0800

    fixed nvim

--- vim/.vimrc
+++ vim/.vimrc
@@ -67,12 +67,14 @@ let &t_8f = "\<Esc>[38;2;%lu;%lu;%lum"
 let &t_8b = "\<Esc>[48;2;%lu;%lu;%lum""]"
 
 
-let hr = (strftime('%H'))
-if hr >= 8 
-    let ayucolor="mirage"
+"let hr = (strftime('%H'))
+"if hr >= 8 
+"    let ayucolor="mirage"
 "elseif hr >= 8
-    "let ayucolor="light"
-elseif hr >= 0
-    let ayucolor="dark" 
-endif
-colorscheme ayu
+"    "let ayucolor="light"
+"elseif hr >= 0
+"    let ayucolor="dark" 
+"endif
+
+" change color theme
+"colorscheme ayu
--- vim/setup_vim.ps1
+++ vim/setup_vim.ps1
@@ -1,11 +1,13 @@
 iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
     ni "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
 mkdir $env:USERPROFILE/.config/nvim
-echo $null >>  $env:USERPROFILE/.config/nvim/init.vim
+# echo $null >>  $env:USERPROFILE/.config/nvim/init.vim
+mkdir $env:USERPROFILE\AppData\Local\nvim
+# echo $null >> $env:USERPROFILE\AppData\Local\nvim\init.vim
 
 echo "set runtimepath^=~/.vim runtimepath+=~/.vim/after
-let &packpath=&runtimepath
-source ~/.vimrc" > $env:USERPROFILE/.config/nvim/init.vim
+let &packpath = &runtimepath
+source ~/.vimrc" > $env:USERPROFILE\AppData\Local\nvim\init.vim
 
 git clone https://github.com/5cr009e/vimrc.git $env:USERPROFILE/.vim_runtime
 cmd /c "mklink  %USERPROFILE%\.vimrc %cd%\.vimrc"
